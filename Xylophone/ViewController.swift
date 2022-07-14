2b5aed216c999810df1f91620c59852b8447cfb2
Haeun Kim <gracekim.an@gmail.com>
2022-07-14 15:27:13 +0900

    start

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
-    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        //find sender.color
+        //call changeColor && delay 2 seconds
+        playSound(sounds: sender.currentTitle!);
+    }
+    func playSound(sounds:String) {
+        let url = Bundle.main.url(forResource: sounds , withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
 
+        }
+    // func changeColor by half when pressed
 }
 
